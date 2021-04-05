f52002bd083470041a5199a716ab77a0fd8db0dd
jr92 <jr92main@gmail.com>
2021-04-05 10:38:55 +0200

    recheck

---
+++ act3/act01/aparcar.php
@@ -0,0 +1,5 @@
+<html><body>
+    <?php
+    header("refresh:5; url= main.php");
+    ?>
+</body></html>
---
+++ act3/act01/main.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php
+//PARKING APP
+
+?>
+<h1>MAIN PAGE</h1>
+<br>
+APARCAR
+<input type="radio" name="aparcar" id="aparcar">
+<br>
+RETIRAR
+<input type="radio" name="retirar" id="retirar">
+<br>
+ESTADO
+<input type="radio" name="estado" id="estado">
+</body>
+</html>
+
