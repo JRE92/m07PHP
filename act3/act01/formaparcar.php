4a787e3be0b500c10e7ea7ae05c8457186d7e2a5
jr92 <jr92main@gmail.com>
2021-04-05 16:49:51 +0200

    form

---
+++ act3/act01/formaparcar.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+    <form action="aparcar.php" method="get">
+        Tipo de coche: <input type="text" name="tipo">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
